c4976c0a71d35d4c132bd71b3015e249d72be43c
Jayesh <jayeshjain1152@gmail.com>
2021-08-16 23:32:36 +0530

    design update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 import { useState } from "react";
 
-const bookDB = {
+const movieDB = {
   Action: [
     { name: "Black Panther", rating: "96%" },
     { name: "Avengers: Endgame", rating: "94%" },
@@ -11,7 +11,10 @@ const bookDB = {
 
   Horror: [
     { name: "Get Out", rating: "98%" },
-    { name: "The Cabinet of Dr. Caligari (Das Cabinet des Dr. Caligari)", rating: "99%" },
+    {
+      name: "The Cabinet of Dr. Caligari (Das Cabinet des Dr. Caligari)",
+      rating: "99%"
+    },
     { name: "A Quiet Place", rating: "96%" }
   ],
 
@@ -35,18 +38,18 @@ export default function App() {
   }
   return (
     <div className="App">
-      <h1> 🎥 Best Movies </h1>
-      <p style={{ fontSize: "smaller" }}>
-        
-        Checkout my favorite books. Select a genre to get started
+      <h1 id="heading-text"> 🎥 Best Movies </h1>
+      <p style={{ fontSize: "smaller", textAlign: "center" }}>
+        Movies I Like the most. Select a genre to get started
       </p>
 
       <div>
-        {Object.keys(bookDB).map((genre) => (
+        {Object.keys(movieDB).map((genre) => (
           <button
             onClick={() => genreClickHandler(genre)}
             style={{
               cursor: "pointer",
+              backgroundColor: "#fb923c",
               background: "#E5E7EB",
               borderRadius: "0.5rem",
               padding: "0.5rem  1rem",
@@ -61,21 +64,22 @@ export default function App() {
       <hr />
       <div style={{ textAlign: "left" }}>
         <ul style={{ paddingInlineStart: "0" }}>
-          {bookDB[selectedGenre].map((book) => (
+          {movieDB[selectedGenre].map((movie) => (
             <li
-              key={book.name}
+              key={movie.name}
               style={{
                 listStyle: "none",
                 padding: "1rem",
                 border: "1px solid #D1D5DB",
                 width: "70%",
                 margin: "1rem 0rem",
-                borderRadius: "0.5rem"
+                borderRadius: "0.5rem",
+                backgroundColor: "#fb923c"
               }}
             >
               {" "}
-              <div style={{ fontSize: "larger" }}> {book.name} </div>
-              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+              <div style={{ fontSize: "larger" }}> {movie.name} </div>
+              <div style={{ fontSize: "smaller" }}> {movie.rating} </div>
             </li>
           ))}
         </ul>
